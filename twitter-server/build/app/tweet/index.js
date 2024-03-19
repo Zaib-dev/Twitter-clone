c1819985b840574576fa9bfc012b3919e014a9f6
root <root@Shahzaib>
2024-03-19 10:27:54 +0500

    addedfrontend

--- twitter-server/build/app/tweet/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tweet = void 0;
-const mutations_1 = require("./mutations");
-const resolvers_1 = require("./resolvers");
-const types_1 = require("./types");
-exports.Tweet = { mutations: mutations_1.mutations, resolvers: resolvers_1.resolvers, types: types_1.types };
